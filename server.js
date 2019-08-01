0782db11270d8e4715a365d6aaee273ac9e09a35
Quinton Smith <smith.quinton42@gmail.com>
2019-08-01 10:43:21 -0400

    updated js

---
+++ server.js
@@ -0,0 +1,91 @@
+var express = require("express");
+var path = require("path");
+
+var app = express();
+var PORT = process.env.PORT || 3030;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+var reservations = [
+    {
+    routeName: "111",
+    table: 1,
+    customer_name: "John Smith",
+    phone_number: "123-450-7890",
+    email: "johnsmith@gmail.com",
+    custimer_id: 111,
+    },
+    {
+    routeName: "222",
+    table: 2,
+    customer_name: "John Smith",
+    phone_number: "123-456-7890",
+    email: "johnsmith@gmail.com",
+    custimer_id: 222,
+    }, 
+    {
+    routeName: "333",
+    table: 3,
+    customer_name: "John Smith",
+    phone_number: "123-450-7890",
+    email: "johnsmith@gmail.com",
+    custimer_id: 333,
+    }
+];
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+    res.sendFile(path.join(__dirname, "resturaunt.html"));
+  });
+  
+  app.get("/reserve", function(req, res) {
+    res.sendFile(path.join(__dirname, "reserve.html"));
+  });
+  
+  app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+  
+  // Displays all characters
+  app.get("/api/tables", function(req, res) {
+    return res.json(reservations);
+  });
+  
+  // Displays a single character, or returns false
+  app.get("/api/tables/:custID", function(req, res) {
+    let chosen  = req.params.custID;
+  
+    console.log(chosen);
+  
+    for (var i = 0; i < characters.length; i++) {
+      if (chosen === tables[i].routeName) {
+        return res.json(tables[i]);
+      }
+    }
+  
+    return res.json(false);
+  });
+  
+  // Create New Characters - takes in JSON input
+  app.post("/api/characters", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body parsing middleware
+    var newCharacter = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newCharacter.routeName = newCharacter.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newCharacter);
+  
+    characters.push(newCharacter);
+  
+    res.json(newCharacter);
+  });
+  
+  // Starts the server to begin listening
+  // =============================================================
+  app.listen(PORT, function() {
+    console.log("App listening on PORT " + PORT);
+  });
